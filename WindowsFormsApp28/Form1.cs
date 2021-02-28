7b35c70a6a9624e9a0b57c5b78864355a49c4ee3
kolia4237 <gffjdryn@gmail.com>
2021-02-28 18:08:25 +0200

    Добавьте файлы проекта.

---
+++ WindowsFormsApp28/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System;
+using System.Data.SqlClient;
+using System.Threading;
+using static System.Console;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp28
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            floor= Convert.ToInt32(textBox2.Text);
+            weight=Convert.ToString(textBox1.Text);
+           using (SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-V11FJUF\SQLEXPRESS;Initial Catalog=calculator;User Id=DESKTOP-V11FJUF\Admin;Integrated Security=True;"))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(@"INSERT INTO [dbo].[Customer]([Weigh],[flor]) VALUES(@my_login, @my_type);", connection))
+                    {
+
+                        SqlParameter loginParameter = new SqlParameter("@my_login", floor);
+                        SqlParameter typeParameter = new SqlParameter("@my_type", weight);
+
+                        command.Parameters.Add(loginParameter);
+                        command.Parameters.Add(typeParameter);
+
+                        int count = command.ExecuteNonQuery();
+                        if (count > 0)
+                            WriteLine($"added {count} rows!");
+                        command.Dispose();
+                    }
+                    connection.Close();
+                }
+            MessageBox.Show("Ввод данных в базу данных успешно произашел");
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ WindowsFormsApp28/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System;
+using System.Data.SqlClient;
+using System.Threading;
+using static System.Console;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp28
+{
+    class Program : Form1
+    {
+       
+        static void Main(string[] args)
+        {
+
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
+
